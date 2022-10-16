3928b299ac30c279b2877e1ce7360dee42f95fb2
marcos_nomoko <marcos@nomoko.world>
2022-10-16 16:57:21 +0200

    wip stylign responsive

--- src/layout/Game/index.tsx
+++ src/layout/Game/index.tsx
@@ -1,12 +1,15 @@
 import { HistoricalPanel } from '../../components/molecules/Historical'
 import { StatsPanel } from '../../components/molecules/StatsPanel'
 import { Main } from '../Main'
+import styled from 'styled-components'
+
+const GameContainer = styled.div``
 export const Game = () => {
   return (
-    <div>
+    <GameContainer>
       <Main />
       <StatsPanel />
       <HistoricalPanel />
-    </div>
+    </GameContainer>
   )
 }
