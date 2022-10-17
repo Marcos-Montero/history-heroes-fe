afa202f3ee84375d45c0e05a91553913500e3526
marcos_nomoko <marcos@nomoko.world>
2022-10-17 09:21:19 +0200

    remove unused styled components dep

--- src/components/molecules/PlayerCard/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonEndTurn, PlayerCardContainer } from './style'
-export const PlayerCard = ({ p }: { p: 1 | 2 }) => {
-  return (
-    <PlayerCardContainer p={p} enabled={true}>
-      <h2>Player {p}</h2>
-      <ButtonEndTurn show={false}>End Turn</ButtonEndTurn>
-    </PlayerCardContainer>
-  )
-}
--- src/layout/Game/index.tsx
+++ src/layout/Game/index.tsx
@@ -1,15 +1,13 @@
 import { HistoricalPanel } from '../../components/molecules/Historical'
 import { StatsPanel } from '../../components/molecules/StatsPanel'
 import { Main } from '../Main'
-import styled from 'styled-components'
 
-const GameContainer = styled.div``
 export const Game = () => {
   return (
-    <GameContainer>
+    <div>
       <Main />
       <StatsPanel />
       <HistoricalPanel />
-    </GameContainer>
+    </div>
   )
 }
